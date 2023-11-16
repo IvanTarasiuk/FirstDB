72f8178ef6a48805643004028de1f7d4d7838e9b
vanechka <tarasyuk.education@gmail.com>
2023-11-16 18:33:32 +0300

    First Connect to DB

---
+++ src/main/resources/create.sql
@@ -0,0 +1,6 @@
+create table audit_departament
+(
+    Ревизионный_номер text not null primary key ,
+    Тип_изделия text not null,
+    Адрес_расположения text not null
+);
---
+++ src/main/resources/select.sql
@@ -0,0 +1,2 @@
+select *
+from audit_departament;
